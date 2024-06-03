918940bd0afb306028f6f22e19c4e23263a570e7
Salih T <105732217+niyht@users.noreply.github.com>
2024-06-03 17:03:30 +0300

    roas-calculator.php

--- roas-calculator/roas-calculator.php
+++ roas-calculator/roas-calculator.php
@@ -26,16 +26,22 @@ add_action('wp_enqueue_scripts', 'roas_scripts');
 function roas_shortcode() {
 return '
 <div>
-    Ciro:<input type="number" id="ciro">
-    <br><br><h4 id="baslik-tum-giderler">Tüm Giderler</h4>
-    Mal Maliyetleri: <input type="number" id="mal_gider">
-    Reklam Harcaması:<input type="number" id="reklam_gider">
-    Kredi Kartı Ücretleri:<input type="number" id="kredikarti_gider">
-    Kargo Ücreti:<input type="number" id="kargo_gider">
-    Reklam Yönetim Maliyeti:<input type="number" id="reklamyonetim_gider">
-    <br><br>
+    <label for="ciro">Ciro:</label>
+    <input type="number" id="ciro">
+    <br>
+    <h4 id="baslik-tum-giderler">Tüm Giderler</h4>
+    <label for="mal_gider">Mal Maliyetleri:</label>
+    <input type="number" id="mal_gider"><br>
+    <label for="reklam_gider">Reklam Harcaması:</label>
+    <input type="number" id="reklam_gider"><br>
+    <label for="kredikarti_gider">Kredi Kartı Ücretleri:</label>
+    <input type="number" id="kredikarti_gider"><br>
+    <label for="kargo_gider">Kargo Ücreti:</label>
+    <input type="number" id="kargo_gider"><br>
+    <label for="reklamyonetim_gider">Reklam Yönetim Maliyeti:</label>
+    <input type="number" id="reklamyonetim_gider"><br><br>
     <button type="reset" id="reset_button">Sıfırla</button>
-    <button type="submit" id="hesapla_button">Hesapla</button><br><br>
+    <button type="button" id="hesapla_button">Hesapla</button><br><br>
     <span id="sonucSpani">
     <h6>Sonuç:&nbsp;</h6>
     <h6 id="sonuc"></h6>
