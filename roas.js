be1172679925a277fe0efb3386f560bda9d1d080
Salih T <105732217+niyht@users.noreply.github.com>
2024-06-03 18:51:30 +0300

    Update roas.js

--- roas.js
+++ roas.js
@@ -1,6 +1,5 @@
 let ciro = document.getElementById("ciro");
 let mal_gider = document.getElementById("mal_gider");
-let reklam_gider = document.getElementById("reklam_gider");
 let kredikarti_gider = document.getElementById("kredikarti_gider");
 let kargo_gider = document.getElementById("kargo_gider");
 let reklamyonetim_gider = document.getElementById("reklamyonetim_gider");
@@ -20,19 +19,19 @@ reset_button.addEventListener("click", () => {
 })
 
 let reklamHarcamasi = (giderler) => {
-    giderler = Number(mal_gider.value) + Number(reklam_gider.value) + Number(kredikarti_gider.value) + Number(kargo_gider.value) + Number(reklamyonetim_gider.value);
+    giderler = Number(mal_gider.value) + Number(kredikarti_gider.value) + Number(kargo_gider.value) + Number(reklamyonetim_gider.value);
     let reklamHarcamasiHesap = Number(ciro.value) - Number(giderler) ;
     let roas = Number(ciro.value) / Number(reklamHarcamasiHesap);
     return roas;
 }
 
 let sonucReklamHarcamasiFonk = (giderler) => {
-    giderler = Number(mal_gider.value) + Number(reklam_gider.value) + Number(kredikarti_gider.value) + Number(kargo_gider.value) + Number(reklamyonetim_gider.value);
+    giderler = Number(mal_gider.value) + Number(kredikarti_gider.value) + Number(kargo_gider.value) + Number(reklamyonetim_gider.value);
     let reklamHarcamasiHesap = Number(ciro.value) - Number(giderler) ;
     return reklamHarcamasiHesap;
 }
 
 hesapla_button.addEventListener("click", () => {
     sonucReklamHarcamasi.innerText =  sonucReklamHarcamasiFonk();
-    sonuc.innerText = "%" + reklamHarcamasi();
+    sonuc.innerText = reklamHarcamasi();
 })
